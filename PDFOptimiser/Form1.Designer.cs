6f6a9227e680fe9450dcd6021892efc50ca12692
twhitbourn <twhitbourn@mcgrathnicol.com>
2017-10-20 13:17:16 +1100

    multi-thread

--- PDFOptimiser/Form1.Designer.cs
+++ PDFOptimiser/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOptimise = new System.Windows.Forms.Button();
+            this.tboxPath = new System.Windows.Forms.TextBox();
+            this.tboxFound = new System.Windows.Forms.TextBox();
+            this.tboxOptimised = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOptimise
+            // 
+            this.btnOptimise.Location = new System.Drawing.Point(599, 175);
+            this.btnOptimise.Name = "btnOptimise";
+            this.btnOptimise.Size = new System.Drawing.Size(75, 23);
+            this.btnOptimise.TabIndex = 0;
+            this.btnOptimise.Text = "Optimise";
+            this.btnOptimise.UseVisualStyleBackColor = true;
+            this.btnOptimise.Click += new System.EventHandler(this.btnOptimiseParallel_Click);
+            // 
+            // tboxPath
+            // 
+            this.tboxPath.Location = new System.Drawing.Point(12, 37);
+            this.tboxPath.Name = "tboxPath";
+            this.tboxPath.Size = new System.Drawing.Size(662, 20);
+            this.tboxPath.TabIndex = 1;
+            // 
+            // tboxFound
+            // 
+            this.tboxFound.Location = new System.Drawing.Point(169, 133);
+            this.tboxFound.Name = "tboxFound";
+            this.tboxFound.Size = new System.Drawing.Size(100, 20);
+            this.tboxFound.TabIndex = 2;
+            // 
+            // tboxOptimised
+            // 
+            this.tboxOptimised.Location = new System.Drawing.Point(430, 133);
+            this.tboxOptimised.Name = "tboxOptimised";
+            this.tboxOptimised.Size = new System.Drawing.Size(100, 20);
+            this.tboxOptimised.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Files found";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Files optimised";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(698, 210);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tboxOptimised);
+            this.Controls.Add(this.tboxFound);
+            this.Controls.Add(this.tboxPath);
+            this.Controls.Add(this.btnOptimise);
+            this.Name = "Form1";
+            this.Text = "PDF Optimiser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOptimise;
+        private System.Windows.Forms.TextBox tboxPath;
+        private System.Windows.Forms.TextBox tboxFound;
+        private System.Windows.Forms.TextBox tboxOptimised;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- PDFOptimiser/Form1.cs
+++ PDFOptimiser/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace PDFOptimiser
 {
@@ -15,6 +21,166 @@ namespace PDFOptimiser
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOptimise_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOptimiseParallel_Click(object sender,EventArgs e)
+        {
+            if (Directory.Exists(tboxPath.Text))
+            {
+                Int64 PDFsFound = 0;
+                Int64 PDFsOptimised = 0;
+
+                // Construct a ConcurrentQueue.
+                ConcurrentQueue<string> cq = new ConcurrentQueue<string>();
+
+                // Populate the queue.
+                foreach (string PDFFile in Directory.EnumerateFiles(tboxPath.Text, "*.pdf", SearchOption.AllDirectories))
+                {
+                    cq.Enqueue(PDFFile);
+                }
+
+                // Start parallel processing
+                Parallel.ForEach<string>(cq, action =>
+                {
+                    string localValue;
+                    Interlocked.Increment(ref PDFsFound);
+                    try
+                    {
+                        while (cq.TryDequeue(out localValue)) gsOptimise(localValue);
+                        Interlocked.Increment(ref PDFsOptimised);
+                    }
+                    catch
+                    {
+
+                    }
+
+                });
+                tboxFound.Text = PDFsFound.ToString();
+                tboxOptimised.Text = PDFsOptimised.ToString();
+                MessageBox.Show("Done");
+            }
+            
+        }
+        private void gsOptimise(string PDFFile, ref Int64 PDFsFound, ref Int64 PDFsOptimised, ref StringBuilder log)
+        {
+            try
+            {
+                PDFsFound++;
+
+                string outputPDF = PDFFile + ".new";
+
+                // Use ProcessStartInfo class
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = true;
+                startInfo.FileName = "gswin32c.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.Arguments = "-q -dBATCH -dNOPAUSE -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dFastWebView=true -sDEVICE=pdfwrite -sOutputFile=\"" + outputPDF + "\" \"" + PDFFile + "\"";
+
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log.AppendLine(PDFFile + "\tGS error: " + ex.Message);
+                }
+
+                if (File.Exists(outputPDF) && File.Exists(PDFFile))
+                {
+                    File.Move(PDFFile, PDFFile + ".old");
+                    File.Move(outputPDF, PDFFile);
+                }
+
+                PDFsOptimised++;
+            }
+            catch (Exception ex)
+            {
+                log.AppendLine(PDFFile + "\t" + ex.Message);
+            }
+        }
+        private void gsOptimise(string PDFFile)
+        {
+            try
+            {
+       
+                string outputPDF = PDFFile + ".new";
+
+                // Use ProcessStartInfo class
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = true;
+                startInfo.FileName = "gswin32c.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.Arguments = "-q -dBATCH -dNOPAUSE -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dFastWebView=true -sDEVICE=pdfwrite -sOutputFile=\"" + outputPDF + "\" \"" + PDFFile + "\"";
+
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //log.AppendLine(PDFFile + "\tGS error: " + ex.Message);
+                }
+
+                if (File.Exists(outputPDF) && File.Exists(PDFFile))
+                {
+                    File.Move(PDFFile, PDFFile + ".old");
+                    File.Move(outputPDF, PDFFile);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //log.AppendLine(PDFFile + "\t" + ex.Message);
+            }
+        }
+        private void iTextOptimise(string PDFFile,ref Int64 PDFsFound,ref Int64 PDFsOptimised,ref StringBuilder log)
+        {
+            try
+                    {
+                PDFsFound++;
+
+                //string outputDirectory = Path.GetDirectoryName(PDFFile) + "_NEW";
+                //if (!Directory.Exists(outputDirectory))
+                //    Directory.CreateDirectory(outputDirectory);
+                //string outputPDF = outputDirectory + "\\" + Path.GetFileName(PDFFile);
+                string outputPDF = PDFFile + ".new";
+                PdfReader reader = new PdfReader(PDFFile);
+                MemoryStream ms = new MemoryStream();
+                PdfStamper stamper = new PdfStamper(reader, new FileStream(outputPDF, FileMode.CreateNew), '7');
+                stamper.SetFullCompression();
+                stamper.Close();
+                ms.Flush();
+                reader.Close();
+
+                if (File.Exists(outputPDF) && File.Exists(PDFFile))
+                {
+                    File.Move(PDFFile, PDFFile + ".old");
+                    File.Move(outputPDF, PDFFile);
+                }
+
+                PDFsOptimised++;
+            }
+                    catch (Exception ex)
+                    {
+                log.AppendLine(PDFFile + "\t" + ex.Message);
+            }
         }
     }
 }
